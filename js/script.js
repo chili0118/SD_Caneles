c32e39fbd0c57575e545a76c0c911b36e9053de7
MiJouHsieh <mijouhsieh@gmail.com>
2023-07-14 23:26:06 +0800

    merge

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-"use strict"
+"use strict";
+
 $('.carousel').slick({
   slidesToShow: 1,  //可以控制每個輪播有幾張圖片
   dots: true,
@@ -11,6 +12,8 @@ $('.carousel').slick({
 });
 
 // ABOUT
+// https://www.google.com/search?q=auto+and+manual+slider&oq=auto+and+manual+slider&aqs=chrome..69i57j0i7i30.24341j0j7&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:a32e4f99,vid:bW8X-tt5AZQ
+
 const slides = document.querySelectorAll(".slide");
 const numberOfSlides = slides.length;
 let slideNumber = 0;
@@ -84,19 +87,20 @@ iconsArea.addEventListener("mouseover", (event) => {
       stopShow();
   }
 
-  let bouncingTexts = target.closest(".icons").children[2].children[0].innerHTML;
+  let bouncingTexts = target.closest(".icons").children[3].children[0].innerHTML;
   let wrapText = ""
   for (let i = 0; i < bouncingTexts.length; i++) {
     wrapText += "<em class='bounce-text'>" + bouncingTexts.charAt(i) + "</em>";
   }
   
-  target.closest(".icons").children[2].children[0].innerHTML= wrapText;
+  target.closest(".icons").children[3].children[0].innerHTML= wrapText;
   let letters = document.getElementsByTagName("em");
   let j = 0;
   
   function applyBounce( ) {
     if (letters[j] !== undefined) {
     setTimeout(function () {
+      letters[j].className = "bounce-text";
       j++;
       if (j < letters.length) {
         applyBounce();
@@ -111,15 +115,15 @@ iconsArea.addEventListener("mouseout", (event) => {
   let target = event.target; //svg
   let iconName =  target.closest(".icons").dataset.name
   if (target.tagName === "svg") {
-    target.closest(".icons").children[2].children[0].innerHTML= iconName
+    target.closest(".icons").children[3].children[0].innerHTML= iconName
     autoplay() 
   }
 });
 
 // FAQ
 
-$('.faq-header').click(function () {
-  $(this).parent('li').toggleClass('faq-active').find('.faq-text').slideToggle();
+$('.faq-list li').click(function () {
+  $(this).toggleClass('faq-active').find('.faq-text').slideToggle();
 });
 
 const shops = [
@@ -199,19 +203,129 @@ function shopsImages(shops) {
 
 bubblesShops.innerHTML = shopsImages(shops);
 
-$(function() {
-  /* 按下GoTop按鈕時的事件 */
-  $('#top').click(function(){
-      $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
-      return false;
+$(function () {
+  $(window).on("scroll", function () {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > 0) {
+      $('#top').fadeIn(500);
+    } else {
+      $('#top').fadeOut(300);
+    }
   });
-  
-  /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
-  $(window).scroll(function() {
-      if ( $(this).scrollTop() > 400){
-          $('#top').fadeIn();
-      } else {
-          $('#top').fadeOut();
-      }
+
+  /* 按下GoTop按鈕時的事件 */
+  $('#top').on("click", function () {
+    $('html,body').stop().animate({ scrollTop: 0 });
   });
 });
+
+const popupData = [
+  // 01
+  {
+    content: `<p>為了讓您的禮物成為最受歡迎的驚喜，我們精心挑選了TOP3可麗露甜點禮盒，帶您一起享受美味的節慶時刻。
+    </p>
+    <p>第一款「甜蜜經典禮盒」，充滿經典口味的可麗露，讓您回味無窮。濃郁的巧克力、絲滑的焦糖和香氣四溢的香草，每一口都散發著節慶的喜悅。</p>
+    <p>第二款「果香盛宴禮盒」，融合了新鮮水果與可麗露的絕妙組合，帶來一場果香四溢的盛宴。搭配著柔順的白巧克力和脆脆的堅果，這款禮盒必定成為舌尖上的驚喜。</p>
+    <p>第三款「精緻節慶禮盒」，獨特的節慶設計，讓您感受到聖誕的熱情。鮮豔的彩糖、可愛的節慶圖案，不僅美味可口，更是一份充滿節日氛圍的禮物。</p>
+    <p>無論是與家人、朋友還是同事，這些可麗露甜點禮盒都是絕佳的選擇，為您的聖誕交換禮物活動增添更多驚喜與甜蜜。立即前往我們的店鋪，一同歡慶這個一年一度的特別時刻，讓可麗露與甜蜜共舞，為您的節日增添珍貴回憶。</p>`
+  },
+  // 02
+  {
+    content: `<p>『感受味蕾的躍動，品味創新的美味』我們驕傲地宣布，期待已久的新口味可麗露即將上市！</p>
+    <p>凝聚了我們烘焙師傅的智慧和創意，推出全新口味帶給您一場前所未有的味覺饗宴。</p>
+    <p>從今天開始，限時三天優惠開跑，讓您在這個夏日盛宴中盡情品味可麗露的奇妙滋味。不僅僅是一份點心，可麗露是一種療癒，是經典與創新的完美結合。
+        我們嚴選最優質的原材料，經過精心烘焙和製作，以確保每一口都帶來最純粹的享受。無論您是巧克力控，還是喜愛水果口味，這次的新品一定能滿足您的味蕾。
+        請即蒞臨我們的店鋪，與我們一同迎接這場美食盛宴。</p>
+    <p>限時三天，錯過就等明年！快來嘗試新口味可麗露，讓您的味覺體驗達到巔峰。</p>`
+  },
+  // 03
+  {
+    content: `<p>絕對不能錯過的口感饗宴，我們誠意推出每日限量的義大利頂級巧克力口味內餡可麗露！這是一場巧克力的盛宴，讓您的味蕾沉醉其中。</p>
+    <p>我們精選了義大利最頂級的巧克力，融合在鬆軟可口的可麗露內餡中。一口咬下，絲滑的巧克力在您的口中綻放，香濃的味道彷彿帶您遨遊於義大利的巧克力之都。</p>
+    <p>每日限量供應，我們堅持以最高品質的巧克力和精湛的製作工藝，確保每一份可麗露都完美無瑕。無論是自己品味，還是與摯愛分享，這款義大利頂級巧克力口味內餡可麗露將為您帶來非凡的味覺享受。</p>
+    <p>別再猶豫了！快來我們的店鋪，每日限量供應，獲取這份珍貴的口感寶藏。讓義大利巧克力的魔力在您的舌尖綻放，帶給您一段美好的味覺旅程。</p>`
+  },
+  // 04
+  {
+    content: `<p>假日來臨，是時候揪好姊妹一同探索台北松山的甜點文創市集啦！這裡集結了眾多手工甜點，讓您盡情享受美味的時刻。</p>
+              <p>無論您是追求傳統口味還是嚮往創新驚喜，這裡都能滿足您的挑剔味蕾。每一個攤位都展示著獨特的手工甜點，色彩繽紛、口感豐富，絕對讓您的舌尖上的味蕾盡情翻飛。</p>
+              <p>帶著您的姊妹們一同穿梭於這個甜點的天堂，品味著一道道令人垂涎欲滴的美食。讓甜點的魔法帶領您進入一個充滿甜蜜和歡笑的世界，為假日增添無限的快樂與回憶。</p>
+              <p>趕緊來台北松山甜點文創市集，與姊妹們一起尋找屬於您的甜蜜時刻。無論是味覺還是情感，這裡都將帶給您驚喜與滿足！</p>`
+  },
+  // 05
+  {
+    content: `<p>踏入法式烘焙的世界，讓我們一同探索可麗露的經典食譜與做法。這款優雅精緻的法式甜點，結合了鬆軟的外皮和濃郁的內餡，定能令您愛不釋手。</p>
+    <p>材料：</p>
+    <ul>
+        <li>125克低筋麵粉</li>
+        <li>15克糖粉</li>
+        <li>2克鹽</li>
+        <li>100毫升牛奶</li>
+        <li>50克無鹽奶油</li>
+        <li>2個全蛋</li>
+        <li>適量巧克力醬或果醬（內餡）</li>
+    </ul>
+    <p>做法：</p>
+    <ul>
+        <li>1. 在碗中混合低筋麵粉、糖粉和鹽，拌勻。</li>
+        <li>2. 在小鍋中加熱牛奶和無鹽奶油，煮至奶油完全融化。</li>
+        <li>3. 將奶油混合物倒入麵粉中，攪拌至順滑。</li>
+        <li>4. 逐個加入蛋，攪拌至完全混合。</li>
+        <li>5. 預熱平底鍋，倒入麵糊，以中小火煎烤約2分鐘。</li>
+        <li>6. 翻轉可麗露，再煎烤約1分鐘，直至兩面呈金黃色。</li>
+        <li>7. 取出可麗露，用巧克力醬或果醬填充內餡。</li>
+        <li>8. 享受美味的法式可麗露！</li>
+    </ul>
+    <p>無論是在下午茶時間還是特別的場合，法式可麗露總能為您帶來獨特的美食體驗。用心製作，讓您在家中也能品味到法國的甜蜜滋味。儘情享受這項經典的法式烘焙藝術吧！</p>`
+  }
+];
+
+window.addEventListener('click', function (event) {
+  let popup = document.getElementById("popup");
+  if (event.target == popup) {
+    popup.style.display = "none";
+    closePopup();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  let parentElement = document.body; // 或者選擇更接近按鈕的父元素
+  parentElement.addEventListener('click', function (event) {
+    if (event.target.matches('.news-card')) {
+      event.preventDefault(); // 防止<a>默認行為（跳轉頁面）
+      openPopup();
+      let button = event.target;
+      let title = button.getAttribute('data-title');
+      let imageSrc = button.getAttribute('data-img');
+      let buttonIndex = button.getAttribute('data-index'); // 新增，獲取按鈕的索引
+
+      let popup = document.getElementById("popup");
+      let popupTitle = document.getElementById("popup-title");
+      let popupImage = document.getElementById("popup-image");
+      let popupContent = document.getElementById("popup-content");
+
+      popupTitle.innerHTML = `${title} <span class="close">&times;</span>`;
+      popupImage.src = imageSrc;
+
+      // 根據按鈕索引從popupData中獲取對應內容
+      let content = popupData[buttonIndex].content;
+      popupContent.innerHTML = content;
+
+      popup.style.display = "block";
+    } else if (event.target.matches('.close')) {
+      closePopup();
+    }
+  });
+});
+
+function openPopup() {
+  var popup = document.getElementById("popup");
+  document.body.style.overflow = "hidden";
+  popup.style.display = "block";
+}
+
+function closePopup() {
+  let popup = document.getElementById("popup");
+  document.body.style.overflow = "auto";
+  popup.style.display = "none";
+}
