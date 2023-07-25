7cc389490840c27449bf6bab7b47565453396bdd
chili0118 <70087615+chili0118@users.noreply.github.com>
2023-07-26 00:02:50 +0800

    Add files via upload
    更新第五版，優化無障礙

--- js/form-validation.js
+++ js/form-validation.js
@@ -106,50 +106,24 @@ function validationNewsletter() {
     const newsletterInput = document.querySelector('#newsletter').value.trim()
     const newsletterMsg = document.querySelector("#newsletterMsg");
     const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
     const modalContainer = document.querySelector(".modal-container")
     const popBtn = document.querySelector(".pop-btn")
     const X = document.querySelector(".x")
-
-    if (newsletterInput.match(pattern)) {
-        newsletterForm.classList.add("valid");
-        newsletterForm.classList.remove("invalid");
-        newsletterMsg.style.visibility = "visible";
-        newsletterMsg.innerHTML = "Your Email Address is valid &#10003;";
-        newsletterMsg.style.color = "#00ff00";
-        popBtn.style.visibility = "visible";
-    } else {
-        newsletterForm.classList.remove("valid");
-        newsletterForm.classList.add("invalid");
-        newsletterMsg.style.visibility = "visible";
-        newsletterMsg.innerHTML = "Please enter valid Email Address";
-        newsletterMsg.style.color = "#ff0000";
-        popBtn.style.visibility = "hidden";
-    }
-
-    if (newsletterInput === "") {
-        newsletterForm.classList.remove("valid");
-        newsletterForm.classList.remove("invalid");
-        newsletterMsg.innerHTML = "";
-    }
-
-    popBtn.addEventListener("click", appear);
 
     function appear(event) {
         modalContainer.style.visibility = "visible";
         newsletterMsg.style.visibility = "hidden";
         event.preventDefault()
         document.querySelector('#newsletter').value = ""
+        popBtn.style.visibility = "hidden";
     }
 
-    X.addEventListener("click", disappearX);
     function disappearX() {
         if (newsletterInput.match(pattern)) {
             modalContainer.style.visibility = "hidden";
         }
     }
 
-    modalContainer.addEventListener("click", disappearModalContainer)
     function disappearModalContainer(e) {
         if (newsletterInput.match(pattern)) {
             if (e.target.className === "modal-container") {
@@ -157,4 +131,56 @@ function validationNewsletter() {
             }
         }
     }
+
+    function validateMailPattern() {
+        const newsletterSpace = document.querySelector('#newsletter')
+
+        if (newsletterInput === "") {
+            newsletterForm.classList.remove("valid");
+            newsletterForm.classList.remove("invalid");
+            newsletterMsg.innerHTML = "";
+            modalContainer.style.visibility = "hidden";
+
+            newsletterSpace.addEventListener("keypress", function (event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault()
+                    return
+                }
+            })
+
+        } else if (newsletterInput.match(pattern)) {
+            
+            newsletterForm.classList.add("valid");
+            newsletterForm.classList.remove("invalid");
+            newsletterMsg.style.visibility = "visible";
+            newsletterMsg.innerHTML = "Your Email Address is valid &#10003;";
+            newsletterMsg.style.color = "#00ff00";
+            popBtn.style.visibility = "visible";
+
+            newsletterSpace.addEventListener("keypress", function (event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault()
+                    appear(event)
+                }
+            })
+        } else {
+            newsletterForm.classList.remove("valid");
+            newsletterForm.classList.add("invalid");
+            newsletterMsg.style.visibility = "visible";
+            newsletterMsg.innerHTML = "Please enter valid Email Address";
+            newsletterMsg.style.color = "#ff0000";
+            popBtn.style.visibility = "hidden";
+
+            newsletterSpace.addEventListener("keypress", function (event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault()
+                }
+            })
+        }
+
+    }
+    validateMailPattern()
+    popBtn.addEventListener("click", appear);
+    X.addEventListener("click", disappearX);
+    modalContainer.addEventListener("click", disappearModalContainer)
 }
