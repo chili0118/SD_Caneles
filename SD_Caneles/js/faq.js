8b4d8dfb193d02b28231ac755ee5eda8c61fee05
chili0118 <70087615+chili0118@users.noreply.github.com>
2023-07-29 16:37:17 +0800

    Delete SD_Caneles directory

--- SD_Caneles/js/faq.js
+++
@@ -1,4 +0,0 @@
-$('.faq-item li').on('click', function(){
-    $(this).toggleClass('active').find('.faq-item-body').slideToggle();
-    $(this).find('.faq-item-title img').toggleClass('active');
-})
